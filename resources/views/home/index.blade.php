fdac5b3341967d7eaf521756bd03f165658d27b3
Harry Sitohang <harry@olx.co.id>
2016-12-12 13:21:39 +0700

    update homepage

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,8 +3,22 @@
 @section('content')
     <!-- Main component for a primary marketing message or call to action -->
     <div class="jumbotron">
-        <h1>Navbar example</h1>
-        <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-        <p>To see the difference between static and fixed top navbars, just scroll.</p>
+        <div class="clearfix">
+            <div class="col-lg-4 col-md-12">
+                <h4><span class="glyphicon glyphicon-briefcase" aria-hidden="true"></span> Place</h4>
+                <p>Place your item by customer</p>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <h4><span class="glyphicon glyphicon-hand-down" aria-hidden="true"></span> Pickup</h4>
+                <p>Pickup nearest item by Kurir</p>
+            </div>
+            <div class="col-lg-4 col-md-12">
+                <h4><span class="glyphicon glyphicon-send" aria-hidden="true"></span> Delivered</h4>
+                <p>Deliver customer item to receiver by kurir</p>
+            </div>
+        </div>
     </div>
+    <p class="text-center">
+        Start Now, <a href="{{ route('register') }}">daftar</a> sebagai kurir atau customer.
+    </p>
 @endsection
